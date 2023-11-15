e3021c7a1b014a56528724d2a9ef179f5392b71a
C - 6 <C - 6@WIN-8NBGM5NR6M6>
2023-11-15 21:23:19 +0500

    Добавьте файлы проекта.

---
+++ AbstractClass/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data;
+using System.Data.Common;
+using System.Data.SQLite;
+
+namespace AbstractClass
+{
+    public abstract class DBJob
+    {
+        public DBJob(string constr)
+        {
+            ConnString = constr;    
+        }
+        public DbConnection Conn;
+        public string ConnString { get; set; }
+        public abstract DbConnection GetDbConnection();
+    }//end abstract Class
+
+    public class SQLLiteDBJob : DBJob
+    {
+        private SQLiteConnection _conn;
+        public SQLLiteDBJob(string constr) : base(constr) {}        
+
+        public override DbConnection GetDbConnection()
+        {
+            throw new NotImplementedException();
+            _conn = new SQLiteConnection(ConnString);
+            Conn= _conn;
+            return Conn;
+        }
+    }//end Class
+}
